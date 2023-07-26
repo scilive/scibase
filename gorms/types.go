9d59496214a45c8bcf92cec94dddbc98f91cf279
daqian.zhang <daqian.zhang@g42.ai>
2023-07-26 10:18:36 +0400

    add int64s

--- gorms/types.go
+++ gorms/types.go
@@ -51,3 +51,37 @@ func (s Strings) Contains(value string) bool {
 	}
 	return false
 }
+
+type Int64s []int64
+
+func (s Int64s) GormDataType() string {
+	return "int64s"
+}
+func (Int64s) GormDBDataType(db *gorm.DB, field *schema.Field) string {
+	switch db.Dialector.Name() {
+	case "mysql", "sqlite":
+		return "JSON"
+	case "postgres":
+		return "JSONB"
+	}
+	return "TEXT"
+}
+
+func (s Int64s) Value() (driver.Value, error) {
+	if len(s) == 0 {
+		return nil, nil
+	}
+	return json.Marshal(s)
+}
+
+func (s *Int64s) Scan(value interface{}) error {
+	switch v := value.(type) {
+	case []byte:
+		return json.Unmarshal(v, s)
+	case string:
+		return json.Unmarshal([]byte(v), s)
+	default:
+		return errors.New("not support such type")
+	}
+
+}
