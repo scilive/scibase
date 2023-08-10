d61a5bd206b8ad105cca28d7c58924ec5c815042
daqian.zhang <daqian.zhang@g42.ai>
2023-08-10 14:24:13 +0400

    add languages

---
+++ languages/languages.go
@@ -0,0 +1,15 @@
+package types
+
+import "strings"
+
+type Language string
+
+func (l Language) String() string {
+	return strings.ToLower(string(l))
+}
+
+const (
+	Zh Language = "zh"
+	En Language = "en"
+	Ar Language = "ar"
+)
