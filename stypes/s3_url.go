f40138e1c4a81413ae2a64d54dfe91b4f100419b
daqian.zhang <daqian.zhang@g42.ai>
2023-08-16 10:21:49 +0400

    add empty judgement

--- stypes/s3_url.go
+++ stypes/s3_url.go
@@ -2,15 +2,23 @@ package stypes
 
 import (
 	"encoding/json"
+	"fmt"
 	"path/filepath"
 	"strings"
 
 	"github.com/daqiancode/env"
+	"github.com/scilive/scibase/logs"
 )
 
 type S3Url string
 
 func S3KeyToURL(key, provider, bucketHost, bucket string) string {
+	if key == "" {
+		return ""
+	}
+	if bucketHost == "" {
+		logs.Log.Error().Msg("S3_BUCKET_HOST is empty")
+	}
 	if strings.HasPrefix(key, "http") {
 		return key
 	}
@@ -28,6 +36,7 @@ func (s S3Url) MarshalJSON() ([]byte, error) {
 	provider := env.Get("S3_PROVIDER")
 	bucketHost := env.Get("S3_BUCKET_HOST")
 	bucket := env.Get("S3_BUCKET")
+	fmt.Println("MarshalJSON")
 	return json.Marshal(S3KeyToURL(string(s), provider, bucketHost, bucket))
 }
 
